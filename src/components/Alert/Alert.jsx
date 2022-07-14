9df03c45f0193978bf112ba25a2cdfdea4ac693c
mbugiel656 <mbugiel656@gmail.com>
2022-07-14 13:47:03 +0200

    Add Alert component

---
+++ src/components/Alert/Alert.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { selectIsAlertActive, setAlertState } from "../../features/pdf/pdfSlice"
+
+import './Alert.css'
+
+
+const Alert = ({ text }) => {
+    const dispatch = useDispatch()
+    const isAlertActive = useSelector(selectIsAlertActive)
+
+    useEffect(() => {
+        if (isAlertActive) {
+            setTimeout(() => {
+                dispatch(setAlertState(false))
+            }, 3000)
+        }
+    }, [isAlertActive])
+
+    
+    return (
+        <div className="alert-box w-100 px-2 py-3" style={{ display: isAlertActive ? 'block' : 'none' }}>{text}</div>
+    )
+}
+
+export default Alert
