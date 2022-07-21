fdd0ac79079a21ebdfe575e2f3fa75b5d1afb540
mbugiel656 <mbugiel656@gmail.com>
2022-07-21 13:10:05 +0200

    Add some changes in Input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,13 +1,15 @@
 import './Input.css'
 
 
-const Input = ({ type, value, placeholder, onChange }) => {
+const Input = ({ type, value, placeholder, onFocus, onBlur, onChange }) => {
     return (
         <input 
             type={type} 
             className="input" 
             value={value} 
             placeholder={placeholder} 
+            onFocus={onFocus}
+            onBlur={onBlur}
             onChange={onChange}
         />
     )
