2a3db93c17d42eeb95e354b1c6eb39ee78550b8d
mbugiel656 <mbugiel656@gmail.com>
2022-07-23 12:25:53 +0200

    Add changes in SearchDropdownList component

--- src/components/SearchDropdown/SearchDropdownList.jsx
+++ src/components/SearchDropdown/SearchDropdownList.jsx
@@ -1,49 +1,28 @@
-import { useEffect } from 'react'
-import { useRef } from 'react'
-import { useDispatch } from 'react-redux'
+import { forwardRef } from 'react'
 import { Link } from 'react-router-dom'
-import { setDropdownState } from '../../features/search/searchSlice'
 
 import './SearchDropdownList.css'
 
 
-const SearchDropdownList = ({ isActive, results }) => {
-    const dispatch = useDispatch()
-    const ref = useRef()
-
-    useEffect(() => {
-        const handleSearchDropdownState = (e) => {
-            if (isActive === true && e.target !== ref.current) {
-                dispatch(setDropdownState(false))
-            }
-        }
-
-        window.addEventListener('click', handleSearchDropdownState)
-
-        return () => {
-            window.removeEventListener('click', handleSearchDropdownState)
-        }
-    })
-
-    return (
+const SearchDropdownList = forwardRef(({ isActive, results, onClick }, ref) => (
         <ul ref={ref} className='search-dropdown-list' style={{ visibility: isActive ? 'visible' : 'hidden', opacity: isActive ? '1' : '0' }}>
             {results 
-                ? results && results.map(result => 
-                    <SearchDropdownItem 
-                        key={result.idDrink}
-                        id={result.idDrink}
-                        name={result.strDrink}
-                        onClick={() => dispatch(setDropdownState(false))}
-                    />
-                )
+                ? 
+                    results && results.map(result =>
+                        <SearchDropdownItem
+                            key={result.idDrink}
+                            id={result.idDrink}
+                            name={result.strDrink}
+                            onClick={onClick}
+                        />
+                    )
                 :
-                    <SearchDropdownItem 
-                        name="No results found..."
+                    <SearchDropdownItemUnactive 
+                        name="No results found"
                     />
             }
         </ul>
-    )
-}
+))
 
 const SearchDropdownItem = ({ id, name, onClick }) => {
     return (
@@ -52,6 +31,14 @@ const SearchDropdownItem = ({ id, name, onClick }) => {
                 {name}
             </li>
         </Link>
+    )
+}
+
+const SearchDropdownItemUnactive = ({ name }) => {
+    return (
+        <li className="search-dropdown-list__item">
+            {name}
+        </li>
     )
 }
 
