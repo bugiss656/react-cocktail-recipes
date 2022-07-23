6b0af2fb3176078cbe5f42662e1a37a3a025fe83
mbugiel656 <mbugiel656@gmail.com>
2022-07-23 12:24:04 +0200

    Add changes in Input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,18 +1,19 @@
+import { forwardRef } from 'react'
+
 import './Input.css'
 
 
-const Input = ({ type, value, placeholder, onFocus, onBlur, onChange }) => {
-    return (
-        <input 
-            type={type} 
-            className="input" 
-            value={value} 
-            placeholder={placeholder} 
-            onFocus={onFocus}
-            onBlur={onBlur}
-            onChange={onChange}
-        />
-    )
-}
+const Input = forwardRef(({ type, value, placeholder, onFocus, onBlur, onChange }, ref) => (
+    <input
+        ref={ref}
+        type={type}
+        className="input"
+        value={value}
+        placeholder={placeholder}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onChange={onChange}
+    />
+))
 
 export default Input
